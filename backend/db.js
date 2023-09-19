37ea4f3f2ad6f86a5d4061d5a858fc6cfd870a07
jayasudhakarchinnadurai <jayasudhakar183@gmail.com>
2023-09-19 11:22:35 +0530

    change db

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb+srv://sudhakar:Sudhakar183@cluster0.k26mwha.mongodb.net/customer?retryWrites=true&w=majority",
+mongoose.connect("mongodb+srv://sudhakar:mRABfJheHuvAifqW@cluster0.k26mwha.mongodb.net/customer?retryWrites=true&w=majority",
 { useNewUrlParser:true,UseUnifiedTopology:true})
