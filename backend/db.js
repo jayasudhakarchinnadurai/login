4ce3f8023d384887c13423d08be1c278a9ad5d57
jayasudhakarchinnadurai <jayasudhakar183@gmail.com>
2023-09-21 12:02:22 +0530

    add gitignore

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb+srv://sudhakar:mRABfJheHuvAifqW@cluster0.k26mwha.mongodb.net/customer?retryWrites=true&w=majority",
+mongoose.connect(`mongodb+srv://sudhakar:mRABfJheHuvAifqW@cluster0.k26mwha.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
 { useNewUrlParser:true,UseUnifiedTopology:true})
